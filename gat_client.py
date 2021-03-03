778a13f4b16234165b7df69f492cbdd0de4efaa5
keisuke <keisuke7771120@me.com>
2021-03-03 19:28:10 +0900

    簡易的にmctsを導入

--- gat_client.py
+++ gat_client.py
@@ -1,8 +1,11 @@
 import socket
 import numpy as np
 import itertools
+import random
+import math
 import time
-from game import State
+
+# from game import State
 from pv_mcts import predict
 from dual_network import DN_INPUT_SHAPE
 from pathlib import Path
@@ -10,8 +13,7 @@ from tensorflow.keras.models import load_model
 
 gamma = 0.9
 
-# おそらく不完全情報ガイスター(のstateのみ？)を定義してそれを更新して管理した方がよさげ
-# 不完全情報ガイスターの盤面情報及びそれらの推測値
+# 不完全情報ガイスターの盤面情報及びそれらの推測値を管理
 class II_State:
 
     # クラス変数で駒順を定義
@@ -93,6 +95,7 @@ class II_State:
         else:
             self.living_piece_color = living_piece_color
 
+        # ex) [0.34, (1, 3, 4, 6)]
         # [[推測値A,(パターンAの青駒のtuple表現)],[推測値B,(パターンBの青駒のtuple表現),...]
         if enemy_estimated_num == None:
             # 盤面の推測値を作成(大きい程青らしく、小さい程赤らしい)
@@ -224,6 +227,31 @@ class II_State:
         # 移動後のマスに敵駒が存在する
         return True, id_index
 
+    # ii_stateと非公開情報のパターンからstateを生成
+    def create_state(self, enemy_blue_pattern_tuple):
+        pieces = [0] * 36
+        for blue_id in self.real_my_piece_blue_set:
+            if self.all_piece[blue_id] < 36:
+                pieces[self.all_piece[blue_id]] = 1
+        for red_id in self.real_my_piece_red_set:
+            if self.all_piece[red_id] < 36:
+                pieces[self.all_piece[red_id]] = 2
+
+        enemy_pieces = [0] * 36
+        for blue_id in enemy_blue_pattern_tuple:
+            if self.all_piece[blue_id] < 36:
+                # enemy_piecesには相手目線の盤面番号を格納する必要がある
+                enemy_pieces[35 - self.all_piece[blue_id]] = 1
+        enemy_red_set = {0, 1, 2, 3, 4, 5, 6, 7} - set(enemy_blue_pattern_tuple)
+        for red_id in enemy_red_set:
+            if self.all_piece[red_id] < 36:
+                enemy_pieces[35 - self.all_piece[red_id]] = 2
+
+        # 本来はdepthを管理して与えるべきだが，ii_stateにdepthを
+        # わざわざ持たせる旨味が薄いため固定値とする．ランダムなら100手前後で試合は決まることが多い
+        state = State(pieces, enemy_pieces, 0)
+        return state
+
     # 自分の行動を受けて、次の状態に遷移(移動先に駒が存在しない場合のみ実行)
     def next(self, action_num):
         coordinate_before, coordinate_after = action_to_coordinate(action_num)
@@ -305,6 +333,308 @@ class II_State:
             hr + row + hr + row + hr + row + hr + row + hr + row + hr + row + hr
         ).format(*board_essence)
         return str
+
+
+# 完全情報ガイスターの状態(MCTSの際に使用予定)
+class State:
+    # 初期化
+    def __init__(self, pieces=None, enemy_pieces=None, depth=0):
+
+        self.is_goal = False
+
+        # 駒の配置
+        if pieces != None:
+            self.pieces = pieces
+        else:
+            self.pieces = [0] * 36
+
+        if enemy_pieces != None:
+            self.enemy_pieces = enemy_pieces
+        else:
+            self.enemy_pieces = [0] * 36
+
+        # ターンの深さ(ターン数)
+        self.depth = depth
+
+        # 駒の初期配置
+        if pieces == None or enemy_pieces == None:
+            # 青4赤4z
+            piece_list = [1, 1, 1, 1, 2, 2, 2, 2]
+
+            random.shuffle(piece_list)  # 配置をランダムに
+            self.pieces[25] = piece_list[0]
+            self.pieces[26] = piece_list[1]
+            self.pieces[27] = piece_list[2]
+            self.pieces[28] = piece_list[3]
+            self.pieces[31] = piece_list[4]
+            self.pieces[32] = piece_list[5]
+            self.pieces[33] = piece_list[6]
+            self.pieces[34] = piece_list[7]
+
+            random.shuffle(piece_list)  # 配置をランダムに
+            self.enemy_pieces[25] = piece_list[0]
+            self.enemy_pieces[26] = piece_list[1]
+            self.enemy_pieces[27] = piece_list[2]
+            self.enemy_pieces[28] = piece_list[3]
+            self.enemy_pieces[31] = piece_list[4]
+            self.enemy_pieces[32] = piece_list[5]
+            self.enemy_pieces[33] = piece_list[6]
+            self.enemy_pieces[34] = piece_list[7]
+
+    # 負けかどうか
+    def is_lose(self):
+        if not any(elem == 1 for elem in self.pieces):  # 自分の青駒が存在しないなら負け
+            # print("青喰い")
+            return True
+        if not any(elem == 2 for elem in self.enemy_pieces):  # 敵の赤駒が存在しない(全部取っちゃった)なら負け
+            # print("赤喰い")
+            return True
+        # 前の手でゴールされてたらis_goalがTrueになってる(ような仕様にする)
+        if self.is_goal:
+            # print("ゴール")
+            return True
+        return False
+
+    # 引き分けかどうか
+    def is_draw(self):
+        return self.depth >= 200  # 200手
+
+    # ゲーム終了かどうか
+    def is_done(self):
+        return self.is_lose() or self.is_draw()
+
+    # デュアルネットワークの入力の2次元配列の取得
+    def pieces_array(self):
+        # プレイヤー毎のデュアルネットワークの入力の2次元配列の取得
+        def pieces_array_of(pieces):
+            table_list = []
+            # 青駒(1)→赤駒(2)の順に取得
+            for j in range(1, 3):
+                table = [0] * 36
+                table_list.append(table)
+                for i in range(36):
+                    if pieces[i] == j:
+                        table[i] = 1
+            return table_list
+
+        # デュアルネットワークの入力の2次元配列の取得(自分と敵両方)
+        return [pieces_array_of(self.pieces), pieces_array_of(self.enemy_pieces)]
+
+    # position->0~35
+    # direction->下:0,左:1,上:2,右:3
+    # 駒の移動元と移動方向を行動に変換
+    def position_to_action(self, position, direction):
+        return position * 4 + direction
+
+    # 行動を駒の移動元と移動方向に変換
+    def action_to_position(self, action):
+        return (int(action / 4), action % 4)  # position,direction
+
+    # 合法手のリストの取得
+    def legal_actions(self):
+        actions = []
+        for p in range(36):
+            # 駒の存在確認
+            if self.pieces[p] != 0:
+                # 存在するなら駒の位置を渡して、その駒の取れる行動をactionsに追加
+                actions.extend(self.legal_actions_pos(p))
+        # 青駒のゴール行動は例外的に合法手リストに追加
+        if self.pieces[0] == 1:
+            actions.extend([2])  # 0*4 + 2
+        if self.pieces[5] == 1:
+            actions.extend([22])  # 5*4 + 2
+        return actions
+
+    # 駒ごと(駒1つに着目した)の合法手のリストの取得
+    def legal_actions_pos(self, position):
+        actions = []
+        x = position % 6
+        y = int(position / 6)
+        # 下左上右の順に行動できるか検証し、できるならactionに追加
+        # ちなみにand演算子は左の値を評価して右の値を返すか決める(左の値がTrue系でなければ右の値は無視する)ので、はみ出し参照してIndexErrorにはならない(&だとなる)
+        if y != 5 and self.pieces[position + 6] == 0:  # 下端でない and 下に自分の駒がいない
+            actions.append(self.position_to_action(position, 0))
+        if x != 0 and self.pieces[position - 1] == 0:  # 左端でない and 左に自分の駒がいない
+            actions.append(self.position_to_action(position, 1))
+        if y != 0 and self.pieces[position - 6] == 0:  # 上端でない and 上に自分の駒がいない
+            actions.append(self.position_to_action(position, 2))
+        if x != 5 and self.pieces[position + 1] == 0:  # 右端でない and 右に自分の駒がいない
+            actions.append(self.position_to_action(position, 3))
+        # 青駒のゴール行動の可否は1ターンに1度だけ判定すれば良いので、例外的にlegal_actionsで処理する(ここでは処理しない)
+        return actions
+
+    # 次の状態の取得
+    def next(self, action):
+        # 次の状態の作成
+        state = State(self.pieces.copy(), self.enemy_pieces.copy(), self.depth + 1)
+
+        # position_bef->移動前の駒の位置、position_aft->移動後の駒の位置
+        # 行動を(移動元, 移動方向)に変換
+        position_bef, direction = self.action_to_position(action)
+
+        # 合法手がくると仮定
+        if direction == 1:  # 左
+            position_aft = position_bef - 1
+        elif direction == 3:  # 右
+            position_aft = position_bef + 1
+        elif direction == 0:  # 下
+            position_aft = position_bef + 6
+        elif direction == 2:  # 上
+            if position_bef == 0 or position_bef == 5:  # 0と5の上行動はゴール処理なので先に弾く
+                state.is_goal = True
+                position_aft = position_bef  # position_befを入れて駒の場所を動かさない(勝敗は決しているので下手に動かさない方が良いと考えた)
+            else:
+                position_aft = position_bef - 6
+        else:
+            print("error：next")
+
+        # 実際に駒移動
+        state.pieces[position_aft] = state.pieces[position_bef]
+        state.pieces[position_bef] = 0
+
+        # 移動先に敵駒が存在した場合は取る(比較と値入れどっちが早いかあとで調べて最適化したい)
+        # piecesとenemy_piecesを対応させるには値をひっくり返す必要がある(要素のインデックスは0~35だから、 n->35-n でひっくり返せる)
+        if state.enemy_pieces[35 - position_aft] != 0:
+            state.enemy_pieces[35 - position_aft] = 0
+
+        # 駒の交代(ターンプレイヤが切り替わるため)(pieces <-> enemy_pieces)
+        tmp = state.pieces
+        state.pieces = state.enemy_pieces
+        state.enemy_pieces = tmp
+        return state
+
+    # 先手かどうか
+    def is_first_player(self):
+        return self.depth % 2 == 0
+
+
+# モンテカルロ木探索で各行動の価値を算出
+def mcts_value(state):
+    # モンテカルロ木探索のノード
+    class node:
+        # 初期化
+        def __init__(self, state):
+            self.state = state  # 状態
+            self.w = 0  # 累計価値
+            self.n = 0  # 試行回数
+            self.child_nodes = None  # 子ノード群
+
+        # 評価
+        def evaluate(self):
+            # ゲーム終了時
+            if self.state.is_done():
+                # 勝敗結果で価値を取得
+                value = -1 if self.state.is_lose() else 0  # 負けは-1、引き分けは0
+                # 累計価値と試行回数の更新
+                self.w += value
+                self.n += 1
+                return value
+
+            # 子ノードが存在しない時
+            if not self.child_nodes:
+                # プレイアウトで価値を取得(要調整)
+                value = playout(self.state)
+
+                # 累計価値と試行回数の更新
+                self.w += value
+                self.n += 1
+
+                # 子ノードの展開
+                if self.n == 10:
+                    self.expand()
+                return value
+
+            else:  # 子ノードが存在する時
+                # UCB1が最大の子ノードの評価で価値を取得
+                value = -self.next_child_node().evaluate()
+
+                # 累計価値と試行回数の更新
+                self.w += value
+                self.n += 1
+                return value
+
+        # 子ノードの展開
+        def expand(self):
+            legal_actions = self.state.legal_actions()
+            self.child_nodes = []
+            for action in legal_actions:
+                self.child_nodes.append(node(self.state.next(action)))
+
+        # UCB1が最大の子ノードを取得
+        def next_child_node(self):
+            # 試行回数nが0の子ノードを返す
+            for child_node in self.child_nodes:
+                if child_node.n == 0:
+                    return child_node
+
+            # UCB1の計算
+            t = 0
+            for c in self.child_nodes:
+                t += c.n
+            ucb1_values = []
+            for child_node in self.child_nodes:
+                ucb1_values.append(
+                    -child_node.w / child_node.n
+                    + 2 * (2 * math.log(t) / child_node.n) ** 0.5
+                )
+
+            # UCB1が最大の子ノードを返す
+            return self.child_nodes[argmax(ucb1_values)]
+
+    # ルートノードの生成
+    root_node = node(state)
+    root_node.expand()
+
+    # ルートノードを1000回評価
+    for _ in range(1000):
+        root_node.evaluate()
+
+    # 試行回数の最大値を持つ行動を返す
+    legal_actions = state.legal_actions()
+    value_list = []
+    for child in root_node.child_nodes:
+        value_list.append(child.n)
+    return value_list
+
+
+# ゲームの終端までシミュレート
+def playout(state):
+    if state.is_lose():
+        return -1
+    if state.is_draw():
+        return 0
+    return -playout(state.next(random_action_for_playout(state)))
+
+
+# 最大値のインデックスを返す
+def argmax(v_list):
+    return v_list.index(max(v_list))
+
+
+# プレイアウト用にランダムで行動選択
+def random_action_for_playout(state):
+    legal_actions = state.legal_actions()
+    return legal_actions[random.randint(0, len(legal_actions) - 1)]
+
+
+def mcts_action(model, ii_state):
+    # 推測値が一番高い盤面を選ぶ(要修正)
+    max_value = 0
+    max_index = 0
+    for i, est in enumerate(ii_state.enemy_estimated_num):
+        if max_value < est[0]:
+            max_value = est[0]
+            max_index = i
+    enemy_blue_pattern_tuple = ii_state.enemy_estimated_num[max_index][1]
+
+    # ii_stateからstateを生成
+    state = ii_state.create_state(enemy_blue_pattern_tuple)
+
+    # 生成したstateに対してMCTSを実行(これを各stateに対しても実行)
+    value_list = mcts_value(state)
+
+    # value_listを足し合わせ，もっとも価値の高い行動を選択
+    return state.legal_actions()[argmax(value_list)]
 
 
 # 行動番号を駒の移動元と移動方向に変換
@@ -502,7 +832,7 @@ def reduce_pattern(dead_piece_ID, color_is_blue: bool, ii_state):
 
 # 相手の推測値を使って無難な手を選択
 # 価値が最大の行動番号を返す
-def action_decision(model, ii_state):
+def assuming_all_board_action(model, ii_state):
     a, b, c = DN_INPUT_SHAPE  # (6, 6, 4)
     my_piece_set = set(ii_state.my_piece_list)
     enemy_piece_set = set(ii_state.enemy_piece_list)
@@ -674,7 +1004,32 @@ def from_recv_to_action_num(model, ii_state, beforehand_estimated_num, now_tcp_s
     ii_state.enemy_next(before, now)
 
     # 自分の行動を決定
-    action_num = action_decision(model, ii_state)
+    action_num = assuming_all_board_action(model, ii_state)
+
+    return action_num
+
+
+# from_recv_to_action_numの
+# MCTSを利用して行動する版
+def mcts_from_recv_to_action_num(
+    model, ii_state, beforehand_estimated_num, now_tcp_str
+):
+    # 前回のstrと受けたstrを並べて相手のaction_numを算出
+    before, now = recv_str_to_coordinate(ii_state, now_tcp_str)  # 座標を算出
+    print("b n", before, now)
+    enemy_action_num = calculate_enemy_action_number_from_coordinate(
+        before, now
+    )  # 行動番号を算出
+    print(enemy_action_num)
+
+    # 相手の行動から推測値を更新
+    update_all_predict_num(ii_state, beforehand_estimated_num, enemy_action_num)
+
+    # 相手の行動からii_stateを更新
+    ii_state.enemy_next(before, now)
+
+    # 自分の行動を決定(MCTS)
+    action_num = mcts_action(model, ii_state)
 
     return action_num
 
@@ -696,8 +1051,8 @@ def first_move(model, ii_state):
 
 def main():
     print("Please input a port number that you want to connect.")
-    port = 10000
-    # port = int(input())
+    # port = 10000
+    port = int(input())
 
     path = sorted(Path("./model").glob("*.h5"))[-1]
     model = load_model(str(path))
@@ -745,7 +1100,7 @@ def main():
 
         while 1:
             # 相手の行動からii_stateを更新→推測値の更新→action_numの決定
-            action_num = from_recv_to_action_num(
+            action_num = mcts_from_recv_to_action_num(
                 model, ii_state, beforehand_estimated_num, now_tcp_str
             )
 
